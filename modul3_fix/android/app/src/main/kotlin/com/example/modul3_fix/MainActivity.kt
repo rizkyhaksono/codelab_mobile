9d03bff953b5192a8423a4ec811ddc36fd0dbbf0
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-10-25 12:22:21 +0700

    fix(codelab): modul 3 fixed)

--- modul3_fix/android/app/src/main/kotlin/com/example/modul3_fix/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.modul3_fix
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- modul3_fix/lib/app/modules/articles/bindings/article_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:modul3/app/modules/articles/controllers/article_controller.dart';
-
-class ArticleBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<ArticleController>(() => ArticleController());
-  }
-}
