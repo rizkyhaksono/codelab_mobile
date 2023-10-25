ba35cc23940364a9bb4d6ff45ccf1cb6df38604c
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2023-10-25 20:19:06 +0800

    Feat(todos): change controller and view to getX

--- modul-3/lib/app/modules/todos/controllers/todo_controller.dart
+++ modul-3/lib/app/modules/todos/controllers/todo_controller.dart
@@ -1,27 +1,42 @@
-import 'dart:convert';
+// todo_controller.dart
 
 import 'package:get/get.dart';
 import 'package:modul3/app/data/models/todo.dart';
-
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class TodoController extends GetxController {
   static const String _baseUrl = 'https://jsonplaceholder.typicode.com/todos/5';
 
-  RxBool isLoading = false.obs;
+  var isLoading = false.obs;
+  var hasError = false.obs;
+  var errorMessage = ''.obs;
+  var hasData = false.obs;
+  var todo = Todo(userId: 0, id: 0, title: '', completed: false).obs;
 
   @override
-  void onInit() async {
+  void onInit() {
     super.onInit();
-    await fetchTodos();
+    fetchTodos();
   }
 
-  Future<Todo> fetchTodos() async {
-    final response = await http.get(Uri.parse(_baseUrl));
-    if (response.statusCode == 200) {
-      return Todo.fromJson(json.decode(response.body));
-    } else {
-      throw Exception('Failed to load album');
+  Future<void> fetchTodos() async {
+    isLoading.value = true;
+    try {
+      final response = await http.get(Uri.parse(_baseUrl));
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        todo.value = Todo.fromJson(data);
+        hasData.value = true;
+      } else {
+        hasError.value = true;
+        errorMessage.value = 'Failed to load data';
+      }
+    } catch (error) {
+      hasError.value = true;
+      errorMessage.value = error.toString();
+    } finally {
+      isLoading.value = false;
     }
   }
 }
